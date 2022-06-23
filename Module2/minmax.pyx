4bb609a2878ba9599b2c56afe3e5986bc155d977
GeorgeG518 <ggannon518@gmail.com>
2022-06-22 20:40:09 -0500

    Worked on cython a bit more, module done once its working

--- Module2/minmax.pyx
+++ Module2/minmax.pyx
@@ -1,15 +1,16 @@
 import minmax
 from minmax cimport point, calcMinMaxOfThread, arg 
+import cython
 
 def calculateMinMax(points, max, min,start,numpoints):
 
     calcMinMaxOfThread(& pyarg(points,max,min,start,numpoints)) 
     return
 
-class pnt:
-    def __init__(self,x,y,z):
+cdef class pnt:
+    def __cinit__(self,double x, double y, double z):
         self.cpoint = point(x,y,z)
 
-cdef pyarg(self,pnts, maxptr, minptr, strt, numpts):
-    cdef double* cmin=minptr    
-    return arg(& pnts, &maxptr,cmin, strt,numpts)
+cdef pyarg(pnts, double maxptr, double minptr, int strt, int numpts):  
+    cdef double * cmin = & minptr
+    return arg(& pnts, &maxptr, cmin, strt,numpts)
