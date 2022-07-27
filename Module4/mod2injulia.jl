0d330d08ef8c6af520ca34537f5b0ce9fa60ec51
GeorgeG518 <ggannon518@gmail.com>
2022-07-26 19:50:18 -0500

    WOrked on the C function calls

--- Module4/mod2injulia.jl
+++ Module4/mod2injulia.jl
@@ -1,1 +1,17 @@
-rand = ccall((:rand, "libc.so.6"), Int32 , () )
+
+function genrand3dpt(MIN, MAX)
+    X=(MAX-MIN)*(float(ccall((:rand, "libc.so.6"), Int32 , () ) ) /2147483647) +  MIN
+    Y=(MAX-MIN)*(float(ccall((:rand, "libc.so.6"), Int32 , () ) ) /2147483647) +  MIN
+    Z=(MAX-MIN)*(float(ccall((:rand, "libc.so.6"), Int32 , () ) ) /2147483647) +  MIN
+    return X,Y,Z
+end
+
+function main()
+    ccall((:srand, "libc.so.6"), Int32 , (Int32,), 7 )
+    x,y,z = genrand3dpt(0, 10000)
+    println(x)
+    println(y)
+    println(z)
+end
+
+main()
