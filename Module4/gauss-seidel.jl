4be6f6b34ba4ba05cd7a17abd41025d03ed794e4
George Gannon <ggannon518@gmail.com>
2022-07-31 21:44:03 -0500

    Found a mistake in the routine. Fixed

--- Module4/gauss-seidel.jl
+++ Module4/gauss-seidel.jl
@@ -14,14 +14,14 @@ function main()
     -2x-8y+5z= 5
     3x+5y+10z = -8
     =#
-    
+
     for i=1:50
         x1 = (14-3*x2+3*x3)/8
         x2 = (5+2*x1-5*x3)/(-8)
-        x3 = (-8-3*x1-5*x2)/(-5)
+        x3 = (-8-3*x1-5*x2)/(10)
         x = [x1 x2 x3]
 
-        dx = √(dot(x-oldx, x-oldx))
+        dx = √(dot(x-oldx, x-oldx)) # finding the square root of the dot product of 
         @printf "%d %.4f %.4f %.4f\n" i x1 x2 x3
         if dx < epsilon
             converged = true
